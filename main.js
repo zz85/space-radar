76c66da39e57ea059a45b5a4d906fe3f86174c07
Joshua Koo <zz85nus@gmail.com>
2015-10-06 01:52:01 +0800

    Try d3 sunburst + treemap examples

--- main.js
+++ main.js
@@ -3,16 +3,24 @@ var BrowserWindow = require('browser-window')
 
 require('crash-reporter').start()
 
+var fs = require('fs');
+var main = fs.readFileSync('main.js')
+console.log(main + '');
+
 
 var mainWindow = null
 
 app.on('window-all-closed', function() {
+  console.log('window all closed');
   if (process.platform != 'darwin') {
     app.quit()
   }
 })
 
 app.on('ready', function() {
+
+  console.log('app is ready');
+
   mainWindow = new BrowserWindow({
     width: 880,
     height: 600
@@ -22,8 +30,8 @@ app.on('ready', function() {
 
   mainWindow.openDevTools()
 
-
   mainWindow.on('closed', function() {
+    console.log('window is closed');
     mainWindow = null
   })
 })
