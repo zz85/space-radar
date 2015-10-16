3b8c60f922dfaa4bec3d83d39999d32761654cd4
Joshua Koo <zz85nus@gmail.com>
2015-10-16 09:27:20 +0800

    Use DOM + CSS for breadcrumb renderering

--- js/bipartition_sunburst.js
+++ js/bipartition_sunburst.js
@@ -372,15 +372,9 @@ var b = {
 };
 
 function initializeBreadcrumbTrail() {
-  // Add the svg area.
-  var trail = d3.select("#sequence").append("svg:svg")
-      .attr("width", width)
-      .attr("height", 50)
-      .attr("id", "trail");
-  // Add the label at the end, for the percentage.
-  trail.append("svg:text")
-    .attr("id", "endlabel")
-    .style("fill", "#000");
+  var trail = d3
+    .select("#sequence")
+    .select('div')
 }
 
 // Given a node in a partition layout, return an array of all of its ancestor
@@ -395,34 +389,20 @@ function getAncestors(node) {
   return path;
 }
 
-
-// Generate a string that describes the points of a breadcrumb polygon.
-function breadcrumbPoints(d, i) {
-  var points = [];
-  points.push("0,0");
-  points.push(b.w + ",0");
-  points.push(b.w + b.t + "," + (b.h / 2));
-  points.push(b.w + "," + b.h);
-  points.push("0," + b.h);
-  if (i > 0) { // Leftmost breadcrumb; don't include 6th vertex.
-    points.push(b.t + "," + (b.h / 2));
-  }
-  return points.join(" ");
-}
-
 // Update the breadcrumb trail to show the current sequence and percentage.
 function updateBreadcrumbs(nodeArray, percentageString) {
 
   // Data join; key function combines name and depth (= position in sequence).
-  var g = d3.select("#trail")
-      .selectAll("g")
+  var g = d3
+    .select("#sequence")
+    .select('div')
+      .selectAll("a")
       .data(nodeArray, function(d) { return d.name + d.depth; });
 
   // Add breadcrumb and label for entering nodes.
-  var entering = g.enter().append("svg:g");
-
-  entering.append("svg:polygon")
-      .attr("points", breadcrumbPoints)
+  var entering = g.enter().
+    append('a')
+    .attr('href', '#')
       .style("fill", function(d) {
         console.log(d.depth);
         var h = hue(d.key);
@@ -433,21 +413,13 @@ function updateBreadcrumbs(nodeArray, percentageString) {
         // return colors[d.name];
       });
 
-  entering.append("svg:text")
-      .attr("x", (b.w + b.t) / 2)
-      .attr("y", b.h / 2)
-      .attr("dy", "0.35em")
-      .attr("text-anchor", "middle")
-      .text(function(d) { return d.name; });
-
-  // Set position for entering and updating nodes.
-  g.attr("transform", function(d, i) {
-    return "translate(" + i * (b.w + b.s) + ", 0)";
-  });
+  entering
+    .text(function(d) { return d.name; });
 
   // Remove exiting nodes.
   g.exit().remove();
 
+  /*
   // Now move and update the percentage at the end.
   d3.select("#trail").select("#endlabel")
       .attr("x", (nodeArray.length + 0.5) * (b.w + b.s))
@@ -459,6 +431,7 @@ function updateBreadcrumbs(nodeArray, percentageString) {
   // Make the breadcrumb trail visible, if it's hidden.
   d3.select("#trail")
       .style("visibility", "");
+      */
 
 }
 
