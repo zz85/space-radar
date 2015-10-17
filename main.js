70e39aafd720e97745d2bc5336df15530914963e
Joshua Koo <zz85nus@gmail.com>
2015-10-18 04:31:25 +0800

    Cleanup

--- main.js
+++ main.js
@@ -3,24 +3,17 @@ var BrowserWindow = require('browser-window')
 
 require('crash-reporter').start()
 
-var fs = require('fs');
-var main = fs.readFileSync('main.js')
-console.log(main + '');
-
-
 var mainWindow = null
 
 app.on('window-all-closed', function() {
   console.log('window all closed');
-  if (process.platform != 'darwin') {
+  // if (process.platform != 'darwin') {
     app.quit()
-  }
+  // }
 })
 
 app.on('ready', function() {
-
   console.log('app is ready');
-
   mainWindow = new BrowserWindow({
     width: 880,
     height: 600
@@ -28,10 +21,10 @@ app.on('ready', function() {
 
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
 
-  mainWindow.openDevTools()
+  // mainWindow.openDevTools()
 
   mainWindow.on('closed', function() {
-    console.log('window is closed');
+    // console.log('window is closed');
     mainWindow = null
   })
 })
