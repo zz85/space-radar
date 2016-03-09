bec33eee8e46f7a08462cd77b7b441afb5e197d8
Joshua Koo <zz85nus@gmail.com>
2016-03-10 06:32:37 +0800

    Minor code formatting

--- js/duFromFile.js
+++ js/duFromFile.js
@@ -3,8 +3,8 @@
 
   const fs = require('fs')
   const path = require('path')
-  const readline = require('readline');
-  const zlib = require('zlib');
+  const readline = require('readline')
+  const zlib = require('zlib')
 
   let counter, current_size
 
@@ -18,35 +18,35 @@
   function findChild(children, dirname) {
     // Try to find the directory within the current children
     // Since the file is usually sorted and we push, the last element is likely the directory we seek
-    var clen = children.length;
-    if ( clen> 1 && children[clen-1].name === dirname ) {
-      return clen-1;
+    let clen = children.length;
+    if (clen > 1 && children[clen-1].name === dirname) {
+      return clen - 1
     } else {
       return children.findIndex(function(element) {
         return element.name === dirname;
-      });
+      })
     }
 
   }
 
   function addFileToNode(node, path, size) {
-    var dirname = path.shift();
-    var index = findChild(node.children, dirname);
+    let dirname = path.shift();
+    let index = findChild(node.children, dirname);
 
-    if ( path.length === 0 ) {
+    if (path.length === 0) {
       // Last element is either a new file which we just push (index -1), or
       // a directory which already exists (index != -1) because of du's sorting
       // so we just add the size to it
-      if ( index === -1 ) {
-        node.children.push({name: dirname, size: size});
+      if (index === -1) {
+        node.children.push({ name: dirname, size: size });
       } else {
         node.children[index].size = size;
       }
     } else {
-      if ( index === -1 ) {
+      if (index === -1) {
         // not found, so we push a new one
         node.children.push(
-          addFileToNode({name: dirname, children: []}, path, size)
+          addFileToNode({ name: dirname, children: [] }, path, size)
         );
       } else {
         node.children[index] = addFileToNode(node.children[index], path, size);
@@ -56,29 +56,32 @@
   }
 
   function readFSFromFile(options, done) {
-    let node, instream;
-    node = options.node;
-    node.name = options.parent;
-    node.children = [];
+    let node, instream
+    node = options.node
+    node.name = options.parent
+    node.children = []
 
-    var currentSize = 0;
+    let currentSize = 0
     // Format is "<size><whitespaces><path>"
-    var lineRegex = /^(\d+)\s+([\s\S]*)$/;
+    let lineRegex = /^(\d+)\s+([\s\S]*)$/
 
-    if ( options.parent.endsWith('.gz') ) {
+    if (options.parent.endsWith('.gz')) {
       instream = fs.createReadStream(options.parent).pipe(zlib.createGunzip());
     } else {
       instream = fs.createReadStream(options.parent);
     }
 
-    var rl = readline.createInterface({
+    instream.setEncoding('utf-8');
+
+    let rl = readline.createInterface({
       input: instream,
       terminal: false
     })
+
     rl.on('line', function(line) {
-      var result = line.match(lineRegex);
-      var size = 0 | result[1] * 1024;
-      var path = result[2].split('/');
+      let result = line.match(lineRegex);
+      let size = 0 | result[1] * 1024;
+      let path = result[2].split('/');
       // Depending on how find is used the first element may be empty
       // if the path started with / or a . which we also don't want
       if ( path[0] === '.' || path[0] === '' ) {
@@ -95,7 +98,7 @@
     });
 
     rl.on('close', function() {
-      done();
+      done()
     });
   }
 
