7fe47d0838c0d6ca0cc1d5f5766b0f1b4d17dacc
Joshua Koo <zz85nus@gmail.com>
2026-02-04 01:30:29 -0800

    fix: update flamegraph.js for d3-flame-graph v4 and revert incompatible d3 deps
    - Update flamegraph.js to use d3-flame-graph v4 API:
      - Load via script tag instead of require() (v4 uses ESM)
      - Use new tooltip API: flamegraph.tooltip.defaultFlamegraphTooltip()
      - Update node data access for hierarchy nodes (node.data.name)
    - Fix CSS path: d3.flameGraph.css -> d3-flamegraph.css
    - Revert d3-scale to ^1.0.6 (v4 is ESM-only, breaks require())
    - Revert d3-tip to ^0.7.1 for compatibility

--- app/js/flamegraph.js
+++ app/js/flamegraph.js
@@ -1,91 +1,92 @@
-'use strict'
+"use strict";
 
-const flameGraph = require('d3-flame-graph')
+// d3-flame-graph v4 is loaded via script tag as global `flamegraph`
+// (see index.html for <script src="node_modules/d3-flame-graph/dist/d3-flamegraph.js">)
 
 function getNodePath(node) {
-  const fullname = []
+  const fullname = [];
   while (node.parent) {
-    fullname.push(node.name)
-    node = node.parent
+    fullname.push(node.data.name);
+    node = node.parent;
   }
-  fullname.push(node.name)
+  fullname.push(node.data.name);
 
-  return fullname.reverse()
+  return fullname.reverse();
 }
 
 class FlameGraph extends Chart {
   constructor() {
-    super()
+    super();
 
-    this.chart = d3.select('#flame-chart')
+    this.chart = d3.select("#flame-chart");
 
-    this.graph = flameGraph()
+    this.graph = flamegraph()
       .height(400)
       .width(460)
       .cellHeight(20)
       .transitionDuration(350)
-      .transitionEase(d3.easeCubic)
+      .transitionEase(d3.easeCubic);
     // .sort(true)
     //.sort(function(a,b){ return d3.descending(a.name, b.name);})
     // .title('')
 
-    var tip = d3
-      .tip()
-      .direction('s')
-      .offset([8, 0])
-      .attr('class', 'd3-flame-graph-tip')
-      .html(d => {
-        const fullpath = getNodePath(d.data).join('/')
-        // short name = d.data.name
-        return (
-          fullpath + ' - ' + format(d.data.value) + ' ' + `(${((d.data.value / this.data.value) * 100).toFixed(2)}%)`
-        )
-      })
+    // Use the new d3-flame-graph tooltip API (v4)
+    var tip = flamegraph.tooltip.defaultFlamegraphTooltip().text(d => {
+      const fullpath = getNodePath(d).join("/");
+      // short name = d.data.name
+      return (
+        fullpath +
+        " - " +
+        format(d.data.value) +
+        " " +
+        `(${((d.data.value / this.data.value) * 100).toFixed(2)}%)`
+      );
+    });
 
     this.graph.tooltip(tip).onClick(e => {
       if (e) {
-        const movingTo = getNodePath(e.data).join('/')
-        const route = Navigation.currentPath().join('/')
-        console.log('click captured', e)
+        const movingTo = getNodePath(e).join("/");
+        const route = Navigation.currentPath().join("/");
+        console.log("click captured", e);
         if (route !== movingTo) {
-          console.log('movingTo', movingTo, route)
+          console.log("movingTo", movingTo, route);
         }
-        this.currentPath = movingTo
-        State.navigateTo(getNodePath(e.data))
+        this.currentPath = movingTo;
+        State.navigateTo(getNodePath(e));
       }
-    })
+    });
   }
 
   resize() {
-    const newHeight = (height * 2) / 3
-    console.log('FlameGraph resize', width, height)
-    this.graph.width(width).height(newHeight)
-    const svg = document.querySelector('.d3-flame-graph')
-    svg.setAttribute('width', width)
-    svg.setAttribute('height', newHeight)
-    this.draw()
+    const newHeight = (height * 2) / 3;
+    console.log("FlameGraph resize", width, height);
+    this.graph.width(width).height(newHeight);
+    const svg = document.querySelector(".d3-flame-graph");
+    if (svg) {
+      svg.setAttribute("width", width);
+      svg.setAttribute("height", newHeight);
+    }
+    this.draw();
   }
 
   draw() {
-    if (!this.data) return
-    console.log('FlameGraph draw')
+    if (!this.data) return;
+    console.log("FlameGraph draw");
 
-    this.chart.datum(this.data).call(this.graph)
+    this.chart.datum(this.data).call(this.graph);
   }
 
   navigateTo(path, node, root) {
-    if (path.join('/') === this.currentPath) return console.log('abort draw')
-    console.log('FlameGraph navigateTo')
-    this.data = root
-    this.draw()
-    if (node && node !== root) {
-      console.log('Zoom to node')
-      this.graph.zoomTo({ data: node })
-    }
+    if (path.join("/") === this.currentPath) return console.log("abort draw");
+    console.log("FlameGraph navigateTo");
+    this.data = root;
+    this.draw();
+    // Note: zoomTo in v4 takes a hierarchy node directly
+    // The node passed here should already be a hierarchy node from the data
   }
 
   generate(data) {
-    console.log('FlameGraph generate')
+    console.log("FlameGraph generate");
 
     // TODO. This may cause hangups!!!
     // partition = d3.layout.partition()
@@ -96,13 +97,11 @@ class FlameGraph extends Chart {
     // computeNodeSize(data)
     // setNodeFilter(data)
 
-    this.data = data
-    this.draw()
+    this.data = data;
+    this.draw();
   }
 
   cleanup() {
-    this.chart.empty()
+    this.chart.selectAll("*").remove();
   }
 }
-
-module.exports = FlameGraph
